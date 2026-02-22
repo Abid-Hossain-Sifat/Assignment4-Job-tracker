e44901eb6acf84310a2a080e863c32eebee3b9bc
Abid Hossain Sifat <itzabid006@gmail.com>
2026-02-22 23:26:39 +0600

    complete button toggle

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,47 @@ function calculateCount() {
     rejectCount.innerText = rejectList.length;
 }
 calculateCount();
+
+
+function toggleStyle(id) {
+    const allBtn = document.getElementById('all-btn');
+    const interviewBtn = document.getElementById('interview-btn');
+    const rejectedBtn = document.getElementById('rejected-btn');
+
+    [allBtn, interviewBtn, rejectedBtn].forEach(btn => {
+        btn.classList.remove('btn-primary', 'text-white');
+        btn.classList.add('btn-soft', 'text-slate-500');
+    });
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('btn-soft', 'text-slate-500');
+    selected.classList.add('btn-primary', 'text-white');
+
+    currentStatus = id;
+
+    allCardSection.classList.add('hidden');
+    filterSection.classList.add('hidden');
+    noJobsSection.classList.add('hidden');
+
+    if (id === 'interview-btn') {
+        if (interviewList.length === 0) {
+            noJobsSection.classList.remove('hidden');
+        } else {
+            filterSection.classList.remove('hidden');
+            renderInterview();
+        }
+        jobAvailable.innerText = interviewList.length;
+    } else if (id === 'rejected-btn') {
+        if (rejectList.length === 0) {
+            noJobsSection.classList.remove('hidden');
+        } else {
+            filterSection.classList.remove('hidden');
+            renderReject();
+        }
+        jobAvailable.innerText = rejectList.length;
+    } else {
+        allCardSection.classList.remove('hidden');
+        jobAvailable.innerText = allCardSection.children.length;
+    }
+    calculateCount();
+}
